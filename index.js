bd30c4e75948f2ebe102268a46182dbec1694a31
mercy chepkemoi koech <koechmercyk@gmail.com>
2025-03-06 12:58:17 +0300

    maths lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2; 
 
-// Write your code here
+// Create a variable called random that will generate a random integer greater than 0.
+let random = Math.floor(Math.random() * 100) + 1; 
+
+// Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+let num3 = 104;
+let num4 = 25;
+let mod = num3 % num4; 
+
+// Create a variable called max that finds the highest number in a set; the value returned should be 20.
+let max = Math.max(1, 2, 3, 4, 20); 
